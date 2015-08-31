e64c1fe1b02f1edaa0941cee34ce55a956707f79
Christian Rackerseder <git@echooff.de>
2015-08-31 19:26:03 +0200

    First implementation

---
+++ src/sass.js
@@ -0,0 +1,24 @@
+import 'fetch';
+import sass from 'sass.js';
+
+const scssFetch = load => {
+  return fetch(load.address)
+    .then(response => response.text())
+    .then(css => {
+      return new Promise((resolve, reject) => {
+        sass.compile(css, result => {
+          if (result.status === 0) {
+            const style = document.createElement('style');
+            style.textContent = result.text;
+            style.setAttribute('type', 'text/css');
+            document.getElementsByTagName('head')[0].appendChild(style);
+            resolve();
+          } else {
+            reject();
+          }
+        });
+      });
+    });
+};
+
+export {scssFetch as fetch};
