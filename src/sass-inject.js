1b3e495cabe98742fadd0f354ead078b11bacf0b
Yifeng <f15gdsy@gmail.com>
2015-09-14 17:06:31 +0800

    - Added file uri scheme support

--- src/sass-inject.js
+++ src/sass-inject.js
@@ -29,11 +29,29 @@ const compile = scss => {
 };
 
 const scssFetch = load => {
-  urlBase = load.address;
-  // fetch initial scss file
-  return fetch(urlBase)
-    .then(response => response.text())
-    .then(compile);
+    urlBase = load.address;
+
+    if (urlBase.indexOf('file://') > -1) {
+        return new Promise((resolve, reject) => {
+            let url = urlBase.slice('file://'.length);
+            fs.readFile(url, 'utf8', (err, data) => {
+                console.log(data);
+
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(data);
+                }
+            });
+        }).then(compile);
+    }
+    else {
+        // fetch initial scss file
+        return fetch(urlBase)
+        .then(response => response.text())
+        .then(compile);
+    }
 };
 
 export default scssFetch;
