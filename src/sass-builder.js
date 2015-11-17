600b11e8a70b8b65298c889110ce9d1d45b8a355
Florian Mounier <florian.mounier@kozea.fr>
2015-11-17 16:22:19 +0100

    Remove BOM chars

--- src/sass-builder.js
+++ src/sass-builder.js
@@ -15,6 +15,7 @@ const escape = source => {
     .replace(/[\n]/g, '\\n')
     .replace(/[\t]/g, '\\t')
     .replace(/[\r]/g, '\\r')
+    .replace(/[\ufeff]/g, '')
     .replace(/[\u2028]/g, '\\u2028')
     .replace(/[\u2029]/g, '\\u2029');
 };
