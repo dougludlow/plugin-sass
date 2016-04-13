6dfe952c74bcd49b7f8f203c2b5bb8176e840a36
Kevin C Jones <him@kevincjones.co.uk>
2016-04-13 17:16:38 +0100

    Update resolve-path.js
    This fixes [issue 43](https://github.com/mobilexag/plugin-sass/issues/43) in regards to the explicit file name generation.

--- src/resolve-path.js
+++ src/resolve-path.js
@@ -8,16 +8,18 @@ const resolvePath = (request) => {
     const { previous } = request;
     let { current } = request;
     if (current.substr(0, 5) === 'jspm:') {
-      current = current.replace(/^jspm:/, '') + '.scss';
+      current = current.replace(/^jspm:/, '');
+      if (!current.match(/\.s(c|a)ss/)) current += '.scss';
       System.normalize(current)
         .then(file => resolve(file.replace(/\.js$|\.ts$/, '')))
         .catch(e => reject(e));
     } else {
       const prevBase = path.dirname(previous) + '/';
       const base = (previous === 'stdin') ? request.options.urlBase : paths[previous] || prevBase;
-      const resolved = url.resolve(base, current);
+      let resolved = url.resolve(base, current);
       if (previous !== 'stdin') paths[current] = path.dirname(resolved) + '/';
-      resolve(`${resolved}.scss`);
+      if (!resolved.match(/\.s(c|a)ss/)) resolved += '.scss';
+      resolve(`${resolved}`);
     }
   });
 };
